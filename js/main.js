ee50b4699aa624c39df37401e22ec8d5a35e2057
IronStalker <v_reznichenko@ukr.net>
2016-01-25 13:29:09 +0200

    scrollto added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-$(function () {
+$(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:0,
         nav:true,
         navText: ['',''],
         items:1
-    })
+    });
+
+    $('a.scroll-to').click(function(){
+        var idscroll = $(this).attr('href');
+        $.scrollTo(idscroll, 1000);
+        return false;
+    });
 });
